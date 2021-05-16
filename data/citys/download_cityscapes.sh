cae40684ad9225dd6f016828fd676eeafbaf46d3
TomMao23 <tom23mao@163.com>
2021-05-17 02:54:26 +0800

    修复数据下载

--- data/citys/download_cityscapes.sh
+++ data/citys/download_cityscapes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=myusername&password=mypassword&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=1652573@tongji.edu.cn&password=58545256mzw&submit=Login' https://www.cityscapes-dataset.com/login/
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
