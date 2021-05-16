2d953c2b33053c0ac0557b320e80fd5e6dc9f19c
TomMao23 <tom23mao@163.com>
2021-05-17 00:42:12 +0800

    为colab增加数据下载脚本

--- READMao.md
+++ READMao.md
@@ -1,2 +1,3 @@
 本科毕业设计项目
+python train.py --noautoanchor --data cityscapes_det.yaml --cfg yolov5s_city_seg.yaml --batch-size 12 --epochs 80 --weights weights/yolov5s.pt --workers 4
 
---
+++ data/citys/download_cityscapes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=myusername&password=mypassword&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
